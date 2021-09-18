77678012d670dc2018a836d05d517f71a2a7708f
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-09-18 13:29:01 +0700

    Belajar struktur dasar import

--- src/com/course/javafundamental/basic/Main.java
+++ src/com/course/javafundamental/basic/Main.java
@@ -1,7 +1,16 @@
 package com.course.javafundamental.basic;
 
+import com.course.javafundamental.basic.kendaraan.Kereta;
+import com.course.javafundamental.basic.kendaraan.Mobil;
+import com.course.javafundamental.basic.kendaraan.Motor;
+import com.course.javafundamental.basic.musik.Gitar;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        Gitar.bunyi();
+        Mobil.jumlahBan();
+        Motor.jumlahBan();
+        Kereta.jumlahBan();
     }
 }
