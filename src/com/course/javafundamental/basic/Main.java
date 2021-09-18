4117300727bfbe759dec241aa563bba1d5b306d0
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-09-18 13:56:25 +0700

    menambahkan library (file jar) secara manual ke proyek

--- src/com/course/javafundamental/basic/Main.java
+++ src/com/course/javafundamental/basic/Main.java
@@ -4,13 +4,21 @@ import com.course.javafundamental.basic.kendaraan.Kereta;
 import com.course.javafundamental.basic.kendaraan.Mobil;
 import com.course.javafundamental.basic.kendaraan.Motor;
 import com.course.javafundamental.basic.musik.Gitar;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
 
 public class Main {
     public static void main (String[] args) {
 //        System.out.println("Hello world!");
 //        Gitar.bunyi();
-        Mobil.jumlahBan();
-        Motor.jumlahBan();
-        Kereta.jumlahBan();
+//        Mobil.jumlahBan();
+//        Motor.jumlahBan();
+//        Kereta.jumlahBan();
+
+        Date today = new Date();
+        System.out.println("Hari ini : " +today);
+        Date tomorrow = DateUtils.addDays(today, 1);
+        System.out.println("Besok : "+tomorrow);
     }
 }
