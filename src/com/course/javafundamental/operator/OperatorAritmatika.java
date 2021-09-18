5d277e31411001ed616ad03429ae34de085a54bd
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-09-18 23:03:37 +0700

    Belajar operator dengan menggunakan simbon yang digunakan untuk melakukan suatu operasi tertentu (memanipulasi, mengolah) satu atau lebih variabel.

---
+++ src/com/course/javafundamental/operator/OperatorAritmatika.java
@@ -0,0 +1,30 @@
+package com.course.javafundamental.operator;
+
+public class OperatorAritmatika {
+    public static void main (String [] args) {
+        System.out.println("Operasi Penjumlahan");
+        int hasilPenjumlahan = 5 + 1;
+        System.out.println("Hasil 5 + 1 = "+hasilPenjumlahan);
+        System.out.println();
+
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 4 - 1;
+        System.out.println("Hasil 4 - 1 = "+hasilPengurangan);
+        System.out.println();
+
+        System.out.println("Operasi Perkalian");
+        int hasilPerkalian = 100 * 2;
+        System.out.println("Hasil 100 * 2 = "+hasilPerkalian);
+        System.out.println();
+
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 20 / 2;
+        System.out.println("Hasil 20 / 2 = "+hasilPembagian);
+        System.out.println();
+
+        System.out.println("Operasi Modulus");
+        int hasilModulus = 20 % 2;
+        System.out.println("Hasil 20 modulus 2 = "+hasilModulus);
+        System.out.println();
+    }
+}
